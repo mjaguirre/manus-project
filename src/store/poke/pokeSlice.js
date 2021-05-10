964368a831772c7ff10bacec6ee88d19e75793ad
mjaguirre <mjaguirre@uc.cl>
2021-05-10 02:06:23 -0400

    ✨ feat(pokemons): add pokemon base thunks

--- src/store/poke/pokeSlice.js
+++ src/store/poke/pokeSlice.js
@@ -1,36 +1,70 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-import { getPokeList } from '../../api/poke';
+import { getPokeList, getPokeDetails } from '../../api/poke';
 
 const initialState = {
-  current: null,
-  list: [],
+  detailedList: [],
+  currentList: [],
+  error: '',
+  loading: false,
 };
 
-const fetchProjects = createAsyncThunk(
-  'poke/fetchPokemons',
-  async (_payload, _thunkAPI) => {
-    const response = await getPokeList();
+const fetchPokemons = createAsyncThunk('poke/fetchPokemons', async (_payload, _thunkAPI) => {
+  const response = await getPokeList();
+  return response.data;
+});
+
+const fetchPokemonDetails = createAsyncThunk(
+  'poke/fetchPokemonDetails',
+  async (payload, _thunkAPI) => {
+    const response = await getPokeDetails(payload);
     return response.data;
-  },
+  }
 );
 
 const pokeSlice = createSlice({
   name: 'poke',
   initialState,
   reducers: {
-    setCurrentProject: (state, action) => {
-      state.current = action.payload;
+    setCurrentList: (state, action) => {
+      state.currentList = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
     },
   },
   extraReducers: {
-    [fetchProjects.fulfilled]: (state, action) => {
-      state.list = action.payload;
+    [fetchPokemons.fulfilled]: (state, action) => {
+      state.error = '';
+      state.list = [];
+      state.list = action.payload.results;
+    },
+    [fetchPokemons.rejected]: (state) => {
+      state.loading = false;
+      state.error = 'Hubo un problema recuperando los datos, intenta nuevamente';
+    },
+    [fetchPokemons.loading]: (state) => {
+      state.error = '';
+      state.loading = true;
+    },
+    [fetchPokemonDetails.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.error = '';
+      state.detailedList += action.payload;
+    },
+    [fetchPokemonDetails.rejected]: (state) => {
+      state.loading = false;
+      state.error = 'Hubo un problema recuperando los datos, intenta nuevamente';
     },
   },
 });
 
-export const { setCurrentProject } = pokeSlice.actions;
-export const fetchProjectsThunk = fetchProjects;
-export const projectReducer = pokeSlice.reducer;
+export const { setCurrentList, setLoading, setError } = pokeSlice.actions;
+export const fetchProjectsThunk = fetchPokemons;
+export const fetchDetailsThunk = fetchPokemonDetails;
+export const pokeReducer = pokeSlice.reducer;
